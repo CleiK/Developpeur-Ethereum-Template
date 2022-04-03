5c1f342da0d867e59aff726d6ab8c53be3c838a2
CleiK <29407538+CleiK@users.noreply.github.com>
2022-04-03 19:27:03 +0200

    chapter 4: truffle contract deployments

---
+++ chapter-4-truffle/migrations/2_deploy.js
@@ -0,0 +1,7 @@
+const Voting = artifacts.require("Voting")
+
+module.exports = function(deployer) {
+  deployer.deploy(Voting);
+};
+
+// ganache-cli -m "silver course cost salon bring front review member wall tilt absent motor"
