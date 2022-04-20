49b246dda1d293876c5f94f096db6b094bc6ec2a
CleiK <29407538+CleiK@users.noreply.github.com>
2022-04-20 10:36:14 +0200

    voting dapp truffle project with react box

---
+++ chapter-6-dapp-ci/Voting/readme.md
@@ -0,0 +1,13 @@
+# Voting DApp
+
+## Local
+
+In a separate terminal, launch a local ganache instance
+```
+npm run ganache
+```
+
+Compile and deploy on your local blockchain
+```
+truffle migrate --reset --network development
+```
---
+++ chapter-6-dapp-ci/Voting/truffle-config.js
@@ -0,0 +1,34 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const dotenv = require('dotenv');
+const path = require("path");
+
+dotenv.config();
+
+module.exports = {
+  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+   networks: {
+    development: {
+     host: "127.0.0.1",   
+     port: 8545,
+     network_id: "1337",
+    },
+    ropsten:{
+      provider : function() {return new HDWalletProvider({mnemonic:{phrase:`${process.env.MNEMONIC}`},providerOrUrl:`${process.env.ROPSTEN_URL}`})},
+      network_id:3,
+    },
+    rinkeby:{
+      provider : function() {return new HDWalletProvider({mnemonic:{phrase:`${process.env.MNEMONIC}`},providerOrUrl:`${process.env.RINKEBY_URL}`})},
+      network_id:4,
+    },
+    kovan:{
+      provider : function() {return new HDWalletProvider({mnemonic:{phrase:`${process.env.MNEMONIC}`},providerOrUrl:`${process.env.KOVAN_URL}`})},
+      network_id:42,
+    },
+  },
+
+  compilers: {
+    solc: {
+      version: "0.8.13",
+    }
+  },
+};
