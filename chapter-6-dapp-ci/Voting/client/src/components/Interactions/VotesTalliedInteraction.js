f2e60a5f660f87813427eb5ebbc3d03c24daee8b
CleiK <29407538+CleiK@users.noreply.github.com>
2022-04-24 17:15:58 +0200

    Revert "Clean and redeployed"
    This reverts commit e552a412f4471d4cd3eaa94ec9b77876d816c6e9.

--- chapter-6-dapp-ci/Voting/client/src/components/Interactions/VotesTalliedInteraction.js
+++ chapter-6-dapp-ci/Voting/client/src/components/Interactions/VotesTalliedInteraction.js
@@ -4,16 +4,26 @@ export default class VotesTalliedInteraction extends Component {
   constructor(props) {
     super(props);
     this.state = { winningProposalId: null };
+
+    this.loadWinningProposalId();
   }
 
-
+  loadWinningProposalId = async () => {
+    this.props.contract.methods.winningProposalID().call({ from: this.props.account })
+      .then((result) => {
+        this.setState({winningProposalId: result});
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }
 
   render() {
     return (
       <div>
         <h4>{this.props.contractStatus}</h4>
         <p>{this.props.description}</p>
-        <p>The winning proposal is {this.state.winningProposalId == null ? '...' : this.state.winningProposalId}</p>
+        <p>The winning proposal is {this.state.winningProposalId == null ? '...' : '#' + this.state.winningProposalId + ' !'}</p>
       </div>
     );
   }
